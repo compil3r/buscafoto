9a493d130c6e7887429ff88b69e5cf69bc3907ee
Vitor Hugo Lopes <vtrhugolopes@gmail.com>
2025-05-22 15:30:35 -0300

    tradução mensagens de login

---
+++ resources/lang/pt_BR/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mensagens de Autenticação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes mensagens de idioma são usadas durante a autenticação
+    | para diversas mensagens que precisamos exibir ao usuário.
+    |
+    */
+
+    'failed' => 'Credenciais inválidas. Verifica teu e-mail e senha.',
+    'password' => 'A senha informada está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
+];
